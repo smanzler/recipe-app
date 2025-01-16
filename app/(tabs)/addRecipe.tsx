465f8448dbb6835c2acfa18b32ddc76cd805840f
smanzler <simanzler@gmail.com>
2025-01-16 12:32:44 -0500

    added scrollview

--- app/(tabs)/addRecipe.tsx
+++ app/(tabs)/addRecipe.tsx
@@ -1,26 +1,35 @@
 import { ThemedText } from "@/components/ThemedText";
-import { StyleSheet, TextInput, View } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function AddScreen() {
   return (
-    <View style={styles.container}>
-      <ThemedText style={styles.title}>Add Recipe</ThemedText>
-      <TextInput style={styles.input}/>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={styles.container}>
+        <ThemedText style={styles.title}>Add Recipe</ThemedText>
+        <TextInput style={styles.input} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     paddingTop: 40,
-    padding: 20
+    padding: 20,
   },
   title: {
-    fontSize: 30
+    fontSize: 30,
   },
   input: {
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
     padding: 10,
     borderRadius: 5,
-  }
+  },
 });
