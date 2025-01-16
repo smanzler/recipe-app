9ef6605d109dae56a805eddb6530652bd79b53de
smanzler <simanzler@gmail.com>
2025-01-16 12:36:11 -0500

    fixed index title

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,24 @@
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+import { ThemedText } from "@/components/ThemedText";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+export default function AddScreen() {
   return (
-    <View style={styles.container}>
-      <ThemedText style={styles.title}>Recipe</ThemedText>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={styles.container}>
+        <ThemedText style={styles.title}>Recipe</ThemedText>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20
+    flex: 1,
+    paddingTop: 40,
+    padding: 20,
   },
   title: {
-    fontSize: 30
-  }
+    paddingTop: 10,
+    fontSize: 30,
+  },
 });
