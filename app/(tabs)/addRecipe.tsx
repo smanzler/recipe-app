236b488be704c2dbeb61d217b194e2ec65524de2
smanzler <simanzler@gmail.com>
2025-01-16 12:26:11 -0500

    added input

--- app/(tabs)/addRecipe.tsx
+++ app/(tabs)/addRecipe.tsx
@@ -1,19 +1,26 @@
 import { ThemedText } from "@/components/ThemedText";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default function AddScreen() {
   return (
     <View style={styles.container}>
-      <ThemedText style={styles.title}>Recipe</ThemedText>
+      <ThemedText style={styles.title}>Add Recipe</ThemedText>
+      <TextInput style={styles.input}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 40,
     padding: 20
   },
   title: {
     fontSize: 30
+  },
+  input: {
+    backgroundColor: 'grey',
+    padding: 10,
+    borderRadius: 5,
   }
 });
